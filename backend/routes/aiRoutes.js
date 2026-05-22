45a14e7002e71e512937692b0dc7d9559a96edff
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-22 15:17:47 +0530

    final routes design

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { generateArticle, generateBlogTitles, generateImage, removeBg, removeObj } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitles, generateImage, removeBg, removeObj, reviewResume } from "../controllers/aiController.js";
 import { auth } from "../middlewares/auth.js";
 
 const aiRouter = express.Router();
@@ -9,5 +9,7 @@ aiRouter.post('/blog-title',auth,generateBlogTitles)
 aiRouter.post('/generate-image',auth,generateImage)
 aiRouter.post('/remove-bg',auth,removeBg)
 aiRouter.post('/remove-obj',auth,removeObj)
+aiRouter.post('/review-resume',auth,reviewResume)
+
 
 export default aiRouter;
