0ca4706b7eaa3fd6cb3bb286192fcd8ad00a7b76
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-22 15:23:48 +0530

    Updated Routes

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,15 +1,18 @@
 import express from "express"
 import { generateArticle, generateBlogTitles, generateImage, removeBg, removeObj, reviewResume } from "../controllers/aiController.js";
 import { auth } from "../middlewares/auth.js";
+import {upload} from "../configs/multer.js"
 
 const aiRouter = express.Router();
 
 aiRouter.post('/write-article',auth,generateArticle)
 aiRouter.post('/blog-title',auth,generateBlogTitles)
 aiRouter.post('/generate-image',auth,generateImage)
-aiRouter.post('/remove-bg',auth,removeBg)
-aiRouter.post('/remove-obj',auth,removeObj)
-aiRouter.post('/review-resume',auth,reviewResume)
+aiRouter.post('/remove-bg',upload.single('image'),auth,removeBg)
+aiRouter.post('/remove-obj',upload.single('image'),auth,removeObj)
+aiRouter.post('/review-resume',upload.single('resume'),auth,reviewResume)
+
+
 
 
 export default aiRouter;
